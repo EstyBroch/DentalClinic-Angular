7228086cd887acaa77de978932019b1664a6c431
esty broch <brochesty@gmail.com>
2023-04-27 13:33:46 +0300

    firstCommit

---
+++ src/app/appointment/appointment/appointment.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnInit,EventEmitter,Output } from '@angular/core';
+import { AppointmentModel } from '../appointment.model';
+import { AppointmentService } from '../appointment.service';
+
+@Component({
+  selector: 'appointment',
+  templateUrl: './appointment.component.html',
+  styleUrls: ['./appointment.component.scss']
+})
+
+export class AppointmentComponent implements OnInit {
+
+  constructor(private appointment_service: AppointmentService) { }
+
+  ngOnInit(): void {
+    this.types=this.appointment_service.getTypes()
+    this.statuses=this.appointment_service.getStatuses()
+  }
+@Input()
+appointment:AppointmentModel={id:0,patient:"gg",type:"hh",status:"",time:"00:00",date:"01/01/2020"}
+
+@Output()
+onAppointmentUpdate : EventEmitter<AppointmentModel> = new EventEmitter<AppointmentModel>();
+
+ableSet:boolean=false
+
+types:string[]=[]
+
+statuses:string[]=[]
+
+setAbleSet(bool:boolean){
+  this.ableSet=bool;
+}
+
+save(){
+ this.setAbleSet(false)
+  this.onAppointmentUpdate.emit(this.appointment)
+}
+}
---
+++ src/app/patients/patient-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PatientsComponent } from './patients/patients.component';
+
+const routes: Routes = [
+   {
+    path: '',
+    component: PatientsComponent,
+  },
+   {
+    path:':patientId/appointments',
+    loadChildren: ()=> import('../appointment/appointment.module').then(t => t.AppointmentModule)
+   }
+];
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PatientRoutingModule { }
